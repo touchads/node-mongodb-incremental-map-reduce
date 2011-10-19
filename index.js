ceaa37e095f9b5e01c8c4f3ba03ca1a3c62e31d2
Jacob Wright <jacwright@gmail.com>
2011-10-19 08:53:45 -0600

    Finished basic operation of inc-mapreduce. Not tested.

--- index.js
+++ index.js
@@ -16,47 +16,49 @@ Collection.prototype.mapReduce = function mapReduce(map, reduce, options, callba
 	out.reduce = out.incremental;
 	
 	var interval = out.interval || 10000;
-	options.$orderBy = out.sort || {_id: 1};
+	options.$orderBy = { _id: 1 };
 	
 	delete out.incremental;
 	delete out.interval;
-	delete out.sort;
 	
+	
+	runMapReduce.call(this, options);
+	
+	// run the mapreduce at a regular interval
+	setInterval(function() {
+		
+	}, interval);
+};
+
+function runMapReduce(options) {
 	// pull the metadata if it exists for this collection
 	var collection = this.db.collection('__incmapreduce__');
 	var metaId = this.collectionName + ':' + out.reduce;
 	
-	collection.find({ _id: metaId }, function(err, result) {
-		if (result) {
-			options.$query = {};
-			for (var i in options.$orderBy) {
-				// TODO determine this result structure and fix the line accordingly
-				options.$query[i] = { $gt: result[i] || 0 };
-			}
+	collection.find({ _id: metaId }, function(err, results) {
+		if (results.length) {
+			options.$query = { _id: { $gt: results[0].lastId }};
 		}
 		
+		// get the max id at this point in time so we can reliably store the last id of the batch
+		var find = { $orderBy: { _id: -1 }, $limit: 1 };
 		
-		// run the mapreduce at a regular interval
-		setInterval(function() {
-			// get the max id at this point in time so we can reliably mapreduce
-			// TODO fix this orderby and select fields
-			var find = { $query:options.$query, $orderBy: reverse.$orderBy, $limit: 1 }, { /* only orderBy fields */};
-			this.find(find, function(err, results) {
-				// TODO results needs to be what we want
-				// TODO add the $lte to the $query 
-				origMapReduce(map, reduce, options, function(err, results) {
-					var lastProcessed = results;
-					
-					if (callback) callback(err, results);
-					
-					if (err) {
-						return;
-					}
-					
-					// TODO find the last reduced items and store their sort field's value here
-					collection.save({ _id: metaId, lastProcessed: lastProcessed });
-				});
+		this.find(find, { _id: 1 }, function(err, results) {
+			if (!results.length) return;
+			
+			var lastId = results[0]._id;
+			var query = options.$query || (options.$query = {});
+			query.$lte = lastId;
+			
+			origMapReduce(map, reduce, options, function(err, results) {
+				if (callback) callback(err, results);
+				
+				if (err) {
+					return;
+				}
+				
+				collection.save({ _id: metaId, lastId: lastId });
 			});
-		}, interval);
+		});
 	});
-};
+}
